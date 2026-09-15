ecf8ffd2b2fb44b0d0006ad622384d2f91fb5264
Kashish Hussain <kashish24hussain@gmail.com>
2026-09-16 00:40:49 +0530

    final skeleton code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 data = {}
-path = "dummy_key_val.txt"
-with open(path, 'r') as f:
+#path = "dummy_key_val.txt"
+with open("dummy_key_val.txt", 'r') as f:
     for line in f:
         key, value = line.split("=", 1)
         data[key.strip()] = value.strip()
 
 def get(key):
-    return data.get(key)
+    print(data.get(key))
 
 def  set(key,value):
     data[key]=value
@@ -15,6 +15,13 @@ def delete(key):
     del data[key]
 
 def write_back(path):
-    file = open(path, 'w')
-    file.write(data)
-    file.close()
+    with open(path, 'w') as f:
+        for key,value in data.items():
+        
+            f.write(f"{key}={value}\n")
+    
+
+set("username", "zoya")
+get("username")
+
+write_back("dummy_key_val.txt")
